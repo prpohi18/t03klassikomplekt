9b53a9faabac15534eca5c95025f1b4a49b31312
Kertu Kipper <phkel@users.noreply.github.com>
2018-02-19 13:08:16 +0200

    kodutöö3: correct tabs

--- Andmed.java
+++ Andmed.java
@@ -1,21 +1,21 @@
 public class Andmed{
   protected String omaniku_eesnimi;
-	protected String omaniku_perekonnanimi;
-	protected String korterinumber;
-	protected String telefoninumber;
+  protected String omaniku_perekonnanimi;
+  protected String korterinumber;
+  protected String telefoninumber;
   protected int soe_vesi; // 3.04
   protected int kulm_vesi; // 2.07
   protected int paev_elekter;  //0.112
   protected int oo_elekter; //0.078
 
   public double veesumma(int soe_vesi, int kulm_vesi){
-  	double vesi=(soe_vesi*3.04)+(kulm_vesi*2.07);
-  		return vesi;	
+    double vesi=(soe_vesi*3.04)+(kulm_vesi*2.07);
+    return vesi;	
   }
     
   public double elektrisumma(int paev_elekter, int oo_elekter){
     double elekter=(paev_elekter*0.112)+(oo_elekter*0.078);
-      return elekter;	
+    return elekter;	
   }
 
   public double kogusumma(int soe_vesi, int kulm_vesi, int paev_elekter, int oo_elekter){
