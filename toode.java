7932b78a34a1a66ee8de917e910e7ec92f81af01
Kalmer Roopa <kalmerr@tlu.ee>
2018-03-12 16:55:54 +0200

    Lisatud kodutöö #3 (ladu)

---
+++ toode.java
@@ -0,0 +1,14 @@
+
+
+
+public class toode {
+    public int Id;
+    public String nimi;
+    public double hind;
+    public double kogus;
+    public double k6ik;
+    
+    void koguhind() {
+    	k6ik = hind * kogus;
+    }
+}
