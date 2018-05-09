babea88f8e0c27c1e272fe8d552cd706f6a14208
Mihkel Jäe <31515689+jaaae@users.noreply.github.com>
2018-05-09 21:54:15 +0300

    kodutoo3

---
+++ kodutoo3/src/main/java/kodutoo3/Loom.java
@@ -0,0 +1,42 @@
+package kodutoo3;
+
+
+public class Loom {
+    private String loomad;
+    private int mass;
+    private int korgus;
+
+
+    public String getLoomad() {
+        return loomad;
+    }
+
+    public void setLoomad(String loomad) {
+        this.loomad = loomad;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public int getKorgus() {
+        return korgus;
+    }
+
+    public void setKorgus(int korgus) {
+        this.korgus = korgus;
+    }
+
+    @Override
+    public String toString() {
+        return "Loomad{" +
+                "loom='" + loomad + '\'' +
+                ", mass=" + mass +
+                ", korgus=" + mass +
+                '}';
+    }
+}
